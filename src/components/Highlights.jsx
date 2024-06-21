325bdd903292d16d74ee0efe70e14a7151c47e63
Mychal Salgado <mychal_salgado@icloud.com>
2024-06-21 09:04:49 -0700

    video carousel component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react'
 import React from 'react'
 import gsap from 'gsap'
 import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
 
 const Highlights = () => {
   useGSAP(() => {
@@ -12,14 +13,13 @@ const Highlights = () => {
     <section id="higlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
         <div className="mb-12 w-full md: flex items-end justify-between">
-          <h1 id="title" className="section-heading">
-            Get the Highlights
-          </h1>
-          <div className="flex flex-wrap items-end gap-5">
-            <p className="link">Watch the film <img src={watchImg} alt="watch" className="ml-2"/> </p>
-            <p className="link">Watch the event <img src={rightImg} alt="right" className="ml-2"/> </p>
-          </div>
+          <h1 id="title" className="section-heading">Get the Highlights</h1>
+            <div className="flex flex-wrap items-end gap-5">
+              <p className="link">Watch the film <img src={watchImg} alt="watch" className="ml-2"/> </p>
+              <p className="link">Watch the event <img src={rightImg} alt="right" className="ml-2"/> </p>
+            </div>
         </div>
+        <VideoCarousel />
       </div>
     </section>
   )
