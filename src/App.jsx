194fa36aecb2aec9ec321eac1186d89b559a7878
Mychal Salgado <mychal_salgado@icloud.com>
2024-08-19 11:39:06 -0700

    how it works + footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import * as Sentry from '@sentry/react'
 import Features from "./components/Features"
+import Footer from "./components/Footer"
+import HowItWorks from "./components/HowItWorks"
 
 const App = () => {  
   return(
@@ -13,6 +15,8 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
